428722c0e5a6ad91f421cd8dfe552c3e7cd55451
aof70C0SgKA <fw2yzyzKhaee4VyE1pJRHe+aof70C0SgKA@noreply.cnb.cool>
2026-08-26 15:46:30 +0800

    fix: 铀燃料棒配方产出对齐官方为10根

--- tools/verify-nuclear.js
+++ tools/verify-nuclear.js
@@ -89,7 +89,7 @@ const ufc = findRecipeObj('RECIPES', 'uranium-fuel-cell');
 checkNum('核燃料棒耗时(10s)', ufc && ufc.time, 10);
 checkNum('核燃料棒消耗铀-235(1)', ufc && ufc.inp['uranium-235'], 1);
 checkNum('核燃料棒消耗铀-238(19)', ufc && ufc.inp['uranium-238'], 19);
-checkNum('核燃料棒产出(1)', ufc && ufc.out['uranium-fuel-cell'], 1);
+checkNum('核燃料棒产出(10)', ufc && ufc.out['uranium-fuel-cell'], 10);
 
 console.log('\n【核燃料（组装机）对齐官方】');
 const nf = findRecipeObj('RECIPES', 'nuclear-fuel');
